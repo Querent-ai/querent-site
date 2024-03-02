be7ec1999aab589dfcdeaf4c6ec5c9c09dfb69b7
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2024-03-02 17:29:49 -0600

    trials

--- src/components/Sections/Section1.svelte
+++ src/components/Sections/Section1.svelte
@@ -1,27 +1,9 @@
 <script lang="ts">
   import SectionWrapper from './SectionWrapper.svelte';
-  import SlideIn from '../SlideIn.svelte';
   import Section1Wave from './Section1Wave.svelte';
   </script>
 
 <SectionWrapper id="section1" classes="lg:px-0">
-  <div
-    class="flex sm:justify-start sm:px-[30px] md:px-[40px] lg:justify-end lg:px-0 vertical-lg:justify-start vertical-lg:px-[30px]"
-  >
-    <section class="flex h-full w-full flex-col gap-6 lg:w-[60%] lg:pl-6">
-      <SlideIn>
-        <h2 class="title-75 paragraph-spacing-left text-teal sm:max-w-[600px] md:max-w-[700px] lg:max-w-[800px]">
-        </h2>
-      </SlideIn>
-      <SlideIn>
-        <p class="subtitle paragraph-spacing-left sm:max-w-[600px] md:max-w-[700px] lg:max-w-[800px]">
-        </p>
-      </SlideIn>
-      <p class="body paragraph-spacing-left sm:max-w-[600px] md:max-w-[700px] lg:max-w-[800px]">
-        Querent is a cutting-edge semantic graph computing platform designed to empower enterprises to extract actionable insights and knowledge from their data. Built with a focus on scalability, flexibility, and efficiency, Querent revolutionizes the way organizations process, analyze, and visualize complex data relationships.
-      </p>
-    </section>
-  </div>
   <div
     class="bottom-0 left-0 w-full sm:-mb-[9px] sm:block sm:h-[190px] md:-mb-[10px] md:h-[220px] lg:absolute lg:mb-0 lg:h-full vertical-lg:relative vertical-lg:-mb-[10px] vertical-lg:block"
   >
--- src/components/Sections/Section1Wave.svelte
+++ src/components/Sections/Section1Wave.svelte
@@ -1,7 +1,5 @@
 <!-- Mobile -->
-<div
-  class={`absolute bottom-0 w-full bg-[url(/src/lib/assets/1_1.svg)] bg-no-repeat sm:right-[10%] sm:h-[180px] sm:bg-right-bottom md:h-[200px] lg:hidden vertical-lg:relative vertical-lg:block`}
-/>
+<div class="absolute bottom-0 w-full bg-no-repeat sm:right-[10%] sm:h-[180px] sm:bg-right-bottom md:h-[200px] lg:hidden vertical-lg:relative vertical-lg:block bg-[url(/src/lib/assets/1_1.svg)] bg-cover bg-center"></div>
 
 <!-- Desktop -->
 <div
